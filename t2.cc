a93193447f27b150b9e74d5c24e361d59f56af6c
yuheng <liyuhengduigong@gmail.com>
2022-07-26 14:39:27 +0930

    changed by li

---
+++ t2.cc
@@ -0,0 +1,27 @@
+//string
+#include<iostream>
+#include<cstring>
+using namespace std;
+/*
+int main (void)
+{
+	cout<<"enter a sting: ";
+	char name[21];
+	cin>>name;
+	cout<<"your name is "<<name<<endl;
+	int len =strlen(name);
+	cout<<"it has "<<len<<" lettes"<<endl;
+	return 0;
+	
+}
+*/
+// cin.getline(vn vn size);
+
+int main (void)
+{
+	char name [51];
+	cout<<"enter your name "<<endl;
+	cin.getline(name,51);
+	cout <<"nice, your name is "<<name<<endl;
+	return 0;
+}
